1a8f186f7e185e59288c0a5fe6b4de971e51d00e
Enas <enasasmar882@gmail.com>
2022-11-27 12:50:10 +0200

    fix

--- src/routes/utility_comment_breadcrumbs.php
+++ src/routes/utility_comment_breadcrumbs.php
@@ -6,7 +6,3 @@ Breadcrumbs::register('utility_comments', function ($breadcrumbs) {
     $breadcrumbs->push(trans('utility-comment::module.comment.title'), url(config('utility-comment.models.comment.resource_url')));
 });
 
-Breadcrumbs::register('utility_category_create_edit', function ($breadcrumbs) {
-    $breadcrumbs->parent('utility_categories');
-    $breadcrumbs->push(view()->shared('title_singular'));
-});
